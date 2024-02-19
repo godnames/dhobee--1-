5dc8207431931571f313fbf8e74f9cb5ef2c9e37
godnames <niranjan.hospitalityminds@gmail.com>
2024-02-19 12:45:26 +0530

    index

---
+++ assets/mail/contact.php
@@ -0,0 +1,40 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the form data
+    $name = $_POST['username'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+	$date = $_POST['date'];
+	$time = $_POST['time'];
+    $message = $_POST['service'];
+
+    // Set the recipient email address
+    $to = "niranjan.hospitalityminds@gmail.com , dhobeellc@gmail.com";
+	
+
+    // Set the email subject
+    $subject = "Contact Form";
+
+    // Set the email headers
+    $headers = "From: $name <$email>" . "\r\n";
+    $headers .= "Reply-To: $email" . "\r\n";
+
+    // Build the email content
+    $emailContent = "Name: $name\n";
+    $emailContent .= "Email: $email\n";
+    $emailContent .= "Phone: $phone\n";
+	$emailContent .= "Date: $date\n";
+	$emailContent .= "Time: $time\n";
+    $emailContent .= "Service: $message\n";
+
+    // Send the email
+    $mailSent = mail($to, $subject, $emailContent, $headers);
+
+    // Check if the email was sent successfully
+    if ($mailSent) {
+        echo "Thank you for your message. We will get back to you soon!";
+    } else {
+        echo "Sorry, there was an error sending your message. Please try again later.";
+    }
+}
+?>
